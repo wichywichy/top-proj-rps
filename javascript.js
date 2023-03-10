ce0c4395db8ea12e5a361b33b2fcf92d2cebbdba
wichywichy <michelleduyenle@gmail.com>
2023-03-09 21:39:33 -0500

    Align html buttons with constants in js. Assign player selection event to click constant.

--- javascript.js
+++ javascript.js
@@ -12,6 +12,73 @@ function getComputerChoice (getRndInteger) {
         console.log ("Scissors");
     }
 }
+//Assign random integer to each variable so that when called, getComputerChoice will choose either Rock, paper, or scissors, at random.
 
-var computerSelection = (getComputerChoice (getRndInteger (1,3)))
+let computerSelection = (getComputerChoice (getRndInteger (1,3)));
+//reduce the function down to a variable for clarity.
 
+const clickRock = document.getElementById("clickRock");
+const clickPaper = document.getElementById("clickPaper");
+const clickScissors = document.getElementById("clickScissors");
+//Align button in html with button constants in java. Declares 3 player select variables.
+
+clickRock.addEventListener ("click", function () {
+    console.log (playRound (clickRock, computerSelection))
+})
+clickPaper.addEventListener ("click", function () {
+    console.log (playRound ())
+})
+clickScissors.addEventListener ("click", function () {
+    console.log (playRound ())
+})
+//Assigns player selection event to click constant.
+
+function playRound () {
+    if (clickRock && computerSelection == "Paper") {
+        return "You lose!";
+    } else if (clickRock && computerSelection == "Scissors") {
+        return "You win!";
+    } else {
+        return "You tie!";
+    }
+}
+
+
+//Tell the computer that player selects a variable, that that is their game choice.
+//Create a function that pits player selection against getComputerChoice.
+
+// let playerSelection = "Rock";
+
+
+
+
+
+// function playRound (playerSelection, computerSelection) {
+    // if (playerSelection === "Rock" && computerSelection === "Paper") {
+       // console.log ("You lose!");
+ //   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+   //     console.log ("You win!");
+   // } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+    //    console.log ("You tie!");
+  //  } else { 
+    //    console.log ("Play again!");
+  //  }
+// }
+
+//function roundOne () {
+
+  //  const playerSelection = 
+    //if (playerSelection === "Rock" && computerSelection === "Paper") {
+      //    console.log ("You lose!");
+     //} else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+       //   console.log ("You win!");
+     // } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+       //   console.log ("You tie!");
+    //  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+      //    console.log ("You win!");
+ //     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+   //       console.log ("You tie!");
+     // } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+      //    console.log ("You lose!");
+//  }   
+// }
