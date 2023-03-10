3a41fad475cc3ce3ee0e9199ee7d06881cf5b90b
wichywichy <michelleduyenle@gmail.com>
2023-03-10 16:34:03 -0500

    Added button click through for game play outside of console.

--- javascript.js
+++ javascript.js
@@ -14,20 +14,14 @@ function getComputerChoice (randomPick) {
         return "Scissors";
     }
 }
-
-const computerSelection = getComputerChoice (randomPick);
-
 //Assign random integer to each variable so that when called, getComputerChoice will choose either Rock, paper, or scissors, at random.
 
-//let computerSelection = getComputerChoice (getRndInteger (1,3);
-//reduce the function down to a variable for clarity.
+const computerSelection = getComputerChoice (randomPick);
 
 const clickRock = document.getElementById("clickRock");
 const clickPaper = document.getElementById("clickPaper");
 const clickScissors = document.getElementById("clickScissors");
 //Align button in html with button constants in java. Declares 3 player select variables.
-
-//Assign playerSelection = Rock, Paper, or Scissors.
 
 function playRound (computerSelection, playerSelection) {
     if (computerSelection == "Paper" && playerSelection == clickRock) {
@@ -46,14 +40,15 @@ function playRound (computerSelection, playerSelection) {
         return "You tie!";
     }
 }
-  //  console.log (playRound (computerSelection, clickScissors))
+//Function created to play a single round.
 
 clickRock.addEventListener ("click", function () {
     console.log (playRound (computerSelection, clickRock))
-})
-clickPaper.addEventListener ("click", function () {
+  })
+  clickPaper.addEventListener ("click", function () {
     console.log (playRound (computerSelection, clickPaper))
-})
-clickScissors.addEventListener ("click", function () {
+  })
+  clickScissors.addEventListener ("click", function () {
     console.log (playRound (computerSelection, clickScissors))
-})
+  })
+  // Added button click through for game play outside of console.
